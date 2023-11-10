7284ba0f6caf0a702b4f440b74f7ba271e705327
Manoj N <146411989+ManojGeniies@users.noreply.github.com>
2023-11-10 10:21:54 +0530

    last commit

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,26 +4,30 @@ import { Link } from "react-router-dom";
 
 const Sidebar = ({ sidebarOpen, w3_close }) => {
   return (
-    <div
-      className={`w3-sidebar w3-bar-block w3-card w3-animate-left ${
-        sidebarOpen ? "w3-open" : ""
-      }`}
-      style={{ width: sidebarOpen ? "25%" : "0" }}
-    >
-      <Link
-        to="learningObjectives"
-        className="w3-bar-item w3-button"
-        id="objectives"
+    <>
+      <div
+        className={`w3-sidebar w3-bar-block w3-card w3-animate-left ${
+          sidebarOpen ? "w3-open" : ""
+        }`}
+        style={{ width: sidebarOpen ? "25%" : "0" }}
       >
-        Learning Objectives
-      </Link>
-      <Link to="introduction" className="w3-bar-item w3-button" id="intro">
-        Introduction
-      </Link>
-      <Link to="types" className="w3-bar-item w3-button" id="thank_you">
-        Thank You
-      </Link>
-    </div>
+        <h1>Menus</h1>
+        <hr className="hrtag"/>
+        <Link
+          to="learningObjectives"
+          className="w3-bar-item w3-button"
+          id="objectives"
+        >
+          Learning Objectives
+        </Link>
+        <Link to="introduction" className="w3-bar-item w3-button" id="intro">
+          Introduction
+        </Link>
+        <Link to="types" className="w3-bar-item w3-button" id="thank_you">
+          Thank You
+        </Link>
+      </div>
+    </>
   );
 };
 
